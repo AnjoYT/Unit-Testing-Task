a2e1401019b0217b91eaf384f9b4928927139671
AnjoYT <AnjoYT@wp.pl>
2024-04-24 20:44:09 +0200

    Task completed

---
+++ UnequalConsecutiveCharacters.Tests/StringAnalyzerTests.cs
@@ -0,0 +1,139 @@
+﻿using Xunit;
+
+namespace UnequalConsecutiveCharacters.Tests
+{
+    public class StringAnalyzerTests
+    {
+        [Theory]
+        [InlineData("aasdfsdf", 7)]
+        [InlineData("AAs%1sdF", 7)]
+        [InlineData("zddfaascdd", 4)]
+        public void UnequalCosecutiveCharacters(string sequence, int expected)
+        {
+            //Act
+            int actual = SequenceAnalyzer.CountUnequalConsecutiveCharacters(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("aa111d3d22s5", 2)]
+        [InlineData("AAs%1sdF", 2)]
+        [InlineData("hhhadfgdsgdd", 3)]
+        public void CosecutiveLetters(string sequence, int expected)
+        {
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveLetters(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData("aa111d3d22s5", 3)]
+        [InlineData("AAs%1sdF", 1)]
+        [InlineData("442355643563333", 4)]
+        public void CosecutiveDigits(string sequence, int expected)
+        {
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveNumbers(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveDigitsReturnsZero()
+        {
+            //Arrange
+            string sequence = "";
+            int expected = 0;
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveNumbers(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveLettersReturnsZero()
+        {
+            //Arrange
+            string sequence = "";
+            int expected = 0;
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveLetters(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+
+        public void UnequalConsecutiveCharReturnsZero()
+        {
+            //Arrange
+            string sequence = "";
+            int expected = 0;
+            //Act
+            int actual = SequenceAnalyzer.CountUnequalConsecutiveCharacters(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveDigitsReturnsOne()
+        {
+            //Arrange
+            string sequence = "1";
+            int expected = 1;
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveNumbers(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveLettersReturnsOne()
+        {
+            //Arrange
+            string sequence = "1";
+            int expected = 1;
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveLetters(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveLettersReturnsSequenceLength()
+        {
+            //Arrange
+            string sequence = "aaaaaaaaa";
+            int expected = 9;
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveLetters(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ConsecutiveDigitsReturnsSequenceLength()
+        {
+            //Arrange
+            string sequence = "111111111";
+            int expected = 9;
+            //Act
+            int actual = SequenceAnalyzer.CountConsecutiveNumbers(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UnequalConsecutiveCharsReturnsSequenceLength()
+        {
+            //Arrange
+            string sequence = "abcdefg";
+            int expected = 7;
+            //Act
+            int actual = SequenceAnalyzer.CountUnequalConsecutiveCharacters(sequence);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ UnequalConsecutiveCharacters/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace UnequalConsecutiveCharacters
+{
+    public class SequenceAnalyzer
+    {
+        public static int CountUnequalConsecutiveCharacters(string sequence)
+        {
+            if (string.IsNullOrEmpty(sequence))
+            {
+                return 0;
+            }
+            sequence = sequence.ToLower();
+            int count = 1;
+            int maxCount = 1;
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (sequence[i] != sequence[i - 1])
+                {
+                    count++;
+                    maxCount = int.Max(count, maxCount);
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            return maxCount;
+        }
+
+        public static int CountConsecutiveLetters(string sequence)
+        {
+            if (string.IsNullOrEmpty(sequence))
+            {
+                return 0;
+            }
+            sequence = sequence.ToLowerInvariant();
+            int count = 1;
+            int maxCount = 1;
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (char.IsLetter(sequence[i]) && char.IsLetter(sequence[i - 1]) && sequence[i] == sequence[i - 1])
+                {
+                    count++;
+                    maxCount = int.Max(count, maxCount);
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+            return maxCount;
+        }
+        public static int CountConsecutiveNumbers(string sequence)
+        {
+            if (string.IsNullOrEmpty(sequence))
+            {
+                return 0;
+            }
+            int count = 1;
+            int maxCount = 1;
+            for (int i = 1; i < sequence.Length; i++)
+            {
+                if (char.IsDigit(sequence[i]) && char.IsDigit(sequence[i - 1]) && sequence[i] == sequence[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+                maxCount = int.Max(count, maxCount);
+            }
+            return maxCount;
+        }
+    }
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
